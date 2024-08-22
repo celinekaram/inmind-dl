94dd319f8516e394498f81842a02598d47462e24
Celine Karam <celineykaram@gmail.com>
2024-08-22 18:01:25 +0300

    Fix structure, checkpoints, argumentParser

--- FinalProject/SemanticSegmentation/train.py
+++ FinalProject/SemanticSegmentation/train.py
@@ -1,13 +1,18 @@
 """
-Main file for training ** model on BMW Semantic dataset
+Main file for training UNET model on BMW Semantic dataset
 """
+import warnings
+
+# Suppress specific warning related to Albumentations version checking
+warnings.filterwarnings("ignore", message="Error fetching version info")
+
 import os
-import matplotlib as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import argparse
 from tqdm import tqdm
-from model import CustomModel
+from UNETmodel import CustomModel
 from torch.utils.tensorboard import SummaryWriter
 from utils import (
     save_checkpoint,
@@ -17,7 +22,6 @@ from utils import (
     save_predictions_as_imgs
 )
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-import datetime
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from utils import calculate_metrics
@@ -27,82 +31,72 @@ torch.manual_seed(seed)
 
 # Hyperparameters
 num_classes = 10
-batch_size = 4
+batch_size = 2
 learning_rate = 2e-5
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-num_epochs = 10
+num_epochs = 1
 num_workers = 2
 pin_memory = True
-
-IMAGE_HEIGHT = 1280 # originally 1280
-IMAGE_WIDTH = 720 # originally 720
-load_model_file = "model_1.pth.tar"
-load_memory = False
+IMAGE_HEIGHT = 720
+IMAGE_WIDTH = 1280
 
 # Define paths
-train_path = 'SemanticSegmentation/data/train/images'
-train_masks_path = 'SemanticSegmentation/data/train/masks'
-val_path = 'SemanticSegmentation/data/val/images'
-val_masks_path = 'SemanticSegmentation/data/val/masks'
-test_path = 'SemanticSegmentation/data/test/images'
-test_masks_path = 'SemanticSegmentation/data/test/masks'
-saved_val_results = 'SemanticSegmentation/val/results'
+train_path = 'SemanticSegmentation/data/train_small/images'
+train_masks_path = 'SemanticSegmentation/data/train_small/masks'
+val_path = 'SemanticSegmentation/data/val_small/images'
+val_masks_path = 'SemanticSegmentation/data/val_small/masks'
+saved_val_results = 'SemanticSegmentation/data/val_small/results'
+
+models_dir = 'SemanticSegmentation/models'
+load_model_file = os.path.join(models_dir, "best.pth")
+load_memory = True
+
+# Create directories if they don't exist
 os.makedirs(saved_val_results, exist_ok=True)
+os.makedirs(models_dir, exist_ok=True)
 
-# Augmentations: normalize, resize, horizontal and vertical flip
-train_transform = A.Compose([
-    A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
-    A.HorizontalFlip(p=0.5),
-    A.VerticalFlip(p=0.2),
-    A.RandomBrightnessContrast(p=0.2),
-    A.Normalize(mean=(0, 0, 0), std=(1, 1, 1), max_pixel_value = 255.0),
-    ToTensorV2(),
-])
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train a semantic segmentation model.")
 
-val_transform = A.Compose([
-    A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
-    A.Normalize(mean = [0.0, 0.0, 0.0], std = [1.0, 1.0, 1.0], max_pixel_value = 255.0),
-    ToTensorV2(),
-])
+    parser.add_argument('--batch_size', type=int, default=batch_size, help='Batch size for training')
+    parser.add_argument('--learning_rate', type=float, default=learning_rate, help='Learning rate for training')
+    parser.add_argument('--best_checkpoint', type=str, default=load_model_file, help='Path to save the best checkpoint')
+    parser.add_argument('--num_epochs', type=int, default=num_epochs, help='Number of epochs to train for')
+    parser.add_argument('--num_workers', type=int, default=num_workers, help='Number of workers for data loading')
+    parser.add_argument('--IMAGE_HEIGHT', type=int, default=IMAGE_HEIGHT, help='Height of input images')
+    parser.add_argument('--IMAGE_WIDTH', type=int, default=IMAGE_WIDTH, help='Width of input images')
 
-def train_fn(train_loader, model, optimizer, loss_fn, scaler):
-    # Metric variables
-    running_loss = 0.0
-    iou_total = 0
-    dice_total = 0
-    precision_total = 0
-    recall_total = 0
-    counter = 0
-    loop = tqdm(train_loader)
-    for step, (image, mask) in enumerate(loop):
-        image = image.to(device)
-        mask = mask.float().unsqueeze(1).to(device)
-        
-        # forward pass
-        with torch.cuda.amp.autocast():
-            prediction = model(image)
-            loss = loss_fn(prediction, mask)
-            running_loss += loss.item()
+    args = parser.parse_args()
+    return args
 
-        # backward pass
-        optimizer.zero_grad()
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
-
-        # update tqdm loop
-        loop.set_postfix(loss=loss.item())
-        
-        # Calculate metrics
-        iou, dice, precision, recall = calculate_metrics(prediction, mask)
-        iou_total += iou
-        dice_total += dice
-        precision_total += precision
-        recall_total += recall
-    return running_loss, iou_total, dice_total, precision_total, recall_total
-        
-        
 def main():
+    args = parse_args()
+
+    # Access the arguments
+    batch_size = args.batch_size
+    learning_rate = args.learning_rate
+    best_checkpoint = args.best_checkpoint
+    num_epochs = args.num_epochs
+    num_workers = args.num_workers
+    image_height = args.IMAGE_HEIGHT
+    image_width = args.IMAGE_WIDTH
+    
+    # Augmentations: normalize, resize, horizontal and vertical flip
+    train_transform = A.Compose([
+        A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.2),
+        A.RandomBrightnessContrast(p=0.2),
+        A.Normalize(mean=(0, 0, 0), std=(1, 1, 1), max_pixel_value = 255.0),
+        ToTensorV2(transpose_mask=True), # transpose 3D input mask from [height, width, num_channels] to [num_channels, height, width]
+    ])
+
+    val_transform = A.Compose([
+        A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+        A.Normalize(mean = [0.0, 0.0, 0.0], std = [1.0, 1.0, 1.0], max_pixel_value = 255.0),
+        ToTensorV2(transpose_mask=True),
+    ])
+
     # Get training and validation data loaders
     train_loader, val_loader = get_loaders(
         train_path,
@@ -122,52 +116,80 @@ def main():
     optimizer = optim.Adam(model.parameters(), lr = learning_rate)
     
     # Setup loss function
-    loss_fn = nn.BCEWithLogitsLoss()
-    
-    # accuracy, iou, training time, inference speed, hyperparams
+    loss_fn = nn.CrossEntropyLoss()
     
     scheduler = ReduceLROnPlateau(optimizer, mode="min", factor=0.1, patience=3, verbose=True)
     
     if load_memory:
-        load_checkpoint(torch.load(load_model_file), model)
-
-    check_accuracy(val_loader, model, device)
+        load_checkpoint(torch.load(best_checkpoint), model)
     
-    log_dir = "./logs" # directory where TensorBoard logs will be stored
+    # Tensorboard
+    log_dir = "SemanticSegmentation/logs" # directory where TensorBoard logs will be stored
     writer = SummaryWriter(log_dir=log_dir) # log data for visualization in TensorBoard
+
     print("Training started")
     # Create training loop
     for epoch in range(num_epochs):
+        model.train() # training mode
+        # Initialize metrics
+        running_loss = 0.0
+        iou_total = 0.0
+        precision_total = 0.0
+        recall_total = 0.0
         print(f"Epoch {epoch+1} start")
-        running_loss, iou_total, dice_total, precision_total, recall_total = train_fn(train_loader, model, optimizer, loss_fn)
-        
-        # Calculate average metrics in each epoch
-        avg_loss = running_loss / len(train_loader)
-        avg_iou = iou_total / len(train_loader)
-        avg_dice = dice_total / len(train_loader)
-        avg_precision = precision_total / len(train_loader)
-        avg_recall = recall_total / len(train_loader)
+        loop = tqdm(train_loader)
+        for step, (image, mask) in enumerate(loop):
+            image = image.to(device) # [batch_size, rgb_channels=3, height, width]
+            mask = mask.float().to(device) # [batch_size, num_classes, height, width]
+            # forward pass
+            prediction = model(image) # [batch_size, num_classes, height, width]
+            loss = loss_fn(prediction, mask)
+            running_loss += loss.item()
+            
+            # backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        # Log metrics to TensorBoard associated with the current epoch number
-        writer.add_scalar("Loss/train", avg_loss, epoch)
-        writer.add_scalar("Metrics/IoU", avg_iou, epoch)
-        writer.add_scalar("Metrics/Dice", avg_dice, epoch)
-        writer.add_scalar("Metrics/Precision", avg_precision, epoch)
-        writer.add_scalar("Metrics/Recall", avg_recall, epoch)
-        
+            # update tqdm loop
+            loop.set_postfix(loss=loss.item())
+            
+            # Calculate metrics
+            iou, precision, recall = calculate_metrics(prediction, mask)
+            iou_total += iou
+            precision_total += precision
+            recall_total += recall
+            
+            # Calculate average metrics in each epoch
+            avg_loss = running_loss / len(train_loader)
+            avg_iou = iou_total / len(train_loader)
+            avg_precision = precision_total / len(train_loader)
+            avg_recall = recall_total / len(train_loader)
+
+            # Log metrics to TensorBoard associated with the current epoch number
+            writer.add_scalar("Loss/train", avg_loss, epoch)
+            writer.add_scalar("Metrics/IoU", avg_iou, epoch)
+            writer.add_scalar("Metrics/Precision", avg_precision, epoch)
+            writer.add_scalar("Metrics/Recall", avg_recall, epoch)
+            
         writer.close()
-        
+            
         # save model
         checkpoint = {
             'state_dict': model.state_dict(),
             'optimizer': optimizer.state_dict(),}
-        save_checkpoint(checkpoint, filename='NEW_MODEL_FILE')
+        try:
+            save_checkpoint(checkpoint, filename=os.path.join(models_dir, f"epoch_{epoch}.pth"))
+        except RuntimeError as e:
+            print(f"Error saving checkpoint: {e}")
+        
+        scheduler.step(avg_loss)
+
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}")
 
         # Check validation accuracy
         val_acc = check_accuracy(val_loader, model, device=device)
         print(f"Validation Accuracy: {val_acc}")
-
-        save_predictions_as_imgs(val_loader, model, folder = saved_val_results, device=device)
 
 if __name__ == "__main__":
     main()
